65851fe561d32fa20a3c9dd01eeb669e63a82cf6
koteitan <ukaibutton@mac.com>
2018-03-25 21:19:47 +0900

    v1.3 was released./ hydra is drawn with packing interval.

--- main.js
+++ main.js
@@ -67,45 +67,109 @@ psh.radius     = 10;
 psh.colshift   = psh.radius*3;
 psh.levelshift = psh.radius*2;
 psh.marginsall = [[10,10],[10,10]]; //[[L,R],[Top,Bottom]]
-psh.marginmatrices = 10;
+psh.marginmatrices = 30;
 psh.fontsize   = 15;
 var drawTree=function(mm){
   var usedsize=[0,0];
   debug.value="";
   switch(drawtype){
     case "pair sequence hydra":
-      //calc size
-      for(mmi=0;mmi<mm.length;mmi++){
+      //calc upperbound and lowerbound of each hydras
+      var matrices = mm.length;
+      var maxcols = 0;
+      for(mmi=0;mmi<matrices;mmi++){
         var m=mm[mmi];
-        var levels = (transpose(m)[0]).max()+1;
-        usedsize[0] = Math.max(usedsize[0] , (m.length+1)*psh.colshift  );
-        usedsize[1] =          usedsize[1] + (levels  +1)*psh.levelshift ;
+        maxcols  = Math.max(maxcols, m.length);
       }
-      usedsize[1] += psh.marginmatrices*(mm.length-1);
-      usedsize[1] += psh.marginsall[1][0]+psh.marginsall[1][1];
-      usedsize[0] += psh.marginsall[0][0]+psh.marginsall[0][1];
+      var upperbound=new Array(matrices);
+      var lowerbound=new Array(matrices);
+      for(mmi=0;mmi<matrices;mmi++){
+        var m=mm[mmi];
+        upperbound[mmi]=new Array(maxcols);
+        lowerbound[mmi]=new Array(maxcols);
+        for(x=0;x<maxcols;x++){
+          upperbound[mmi][x]=1;
+          if(x<m.length){
+            lowerbound[mmi][x]=m[x][0];
+          }else{
+            lowerbound[mmi][x]=+Infinity;
+          }
+        }
+      }
+      for(mmi=0;mmi<matrices;mmi++){
+        var m=mm[mmi];
+        for(cx=0;cx<m.length;cx++){
+          upperbound[mmi][cx] = m[cx][0]+1; //length of root to child
+          var px = findParent(m,cx);
+          if(px>=0){
+            // fill for branch at parent to child in x axis
+            for(cx2=px;cx2<=cx;cx2++){
+              // fill for branch at a level lower than child in y axis (+1 for root)
+              lowerbound[mmi][cx2]=Math.min(lowerbound[mmi][cx2], m[px][0]+1);
+            }
+          }else{
+            // fill for branch at parent(=root) to child in x axis
+            for(cx2=0;cx2<=cx;cx2++){
+              // fill for branch at level 0
+              lowerbound[mmi][cx2]=0;
+            }
+          }//if
+        }//for cx
+      }//for mmi
+
+      //packing and decide position of root
+      //depth[x] = used area of column x
+      var depth=new Array(maxcols);
+      for(x=0;x<maxcols;x++) depth[x]=0;
+      //rooty      [mmi] = y-position of root of mmi th matrix [unit is nodes]
+      //rooty_pixel[mmi] = y-position of root of mmi th matrix [unit is pixels]
+      //rootx_pixel[mmi] = x-position of root of mmi th matrix [unit is pixels]
+      var rooty      =new Array(matrices);
+      var rooty_pixel=new Array(matrices);
+      var rootx_pixel=psh.radius;
+      for(mmi=0;mmi<matrices;mmi++){
+        rooty[mmi] = 1;
+        for(x=0;x<maxcols;x++){
+          rooty[mmi] = Math.max(rooty[mmi], upperbound[mmi][x]+depth[x]+1);
+        }
+        for(x=0;x<maxcols;x++){
+          var prev = depth[x];
+          depth[x] = Math.max(prev, rooty[mmi]-lowerbound[mmi][x]);
+        }
+        rooty_pixel[mmi] = rooty[mmi];
+        rooty_pixel[mmi] *= psh.levelshift;
+        rooty_pixel[mmi] += mmi*psh.marginmatrices;
+      }
+      // decide use size
+      usedsize[0]  = (maxcols+1)*psh.colshift + psh.radius*2;
+      usedsize[0] += psh.marginsall[0][0];
+      usedsize[0] += psh.marginsall[0][1];
+      
+      usedsize[1]  = rooty_pixel[mm.length-1] + psh.levelshift + psh.radius*2;
+      usedsize[1] += psh.marginsall[1][0];
+      usedsize[1] += psh.marginsall[1][1];
       //resize canvas
       outcanvas.width=usedsize[0];
       outcanvas.height=usedsize[1];
-      var root=[psh.marginsall[0][0], psh.marginsall[0][1]];
-      for(mmi=0;mmi<mm.length;mmi++){
+      //draw background
+      ctx.fillStyle="white";
+      ctx.fillRect(0,0,outcanvas.width,outcanvas.height);
+      //draw trees
+      for(mmi=0;mmi<matrices;mmi++){
         var m=mm[mmi];
         var levels = (transpose(m)[0]).max()+1;
-        root[0] =psh.marginsall[0][0] + psh.radius; // no inclement.
-        root[1]+=(levels+1)*psh.levelshift;         // inclement. +1 is for root.
-        if(mmi>0)root[1]+=psh.marginmatrices;       // margin for matrices
         //draw root
         var text   = "r";
-        ctx.strokeStyle='black';
+        ctx.fillStyle='black';
         ctx.font = String(psh.fontsize)+'px Segoe UI';
         textwidth  = ctx.measureText(text).width;
         textheight = psh.fontsize;
-        ctx.fillText(text,root[0]-textwidth/2,root[1]+textheight/2*0.7);
+        ctx.fillText(text,rootx_pixel-textwidth/2,rooty_pixel[mmi]+textheight/2*0.7);
         //draw columns
         for(ci=0;ci<m.length;ci++){
           var level  = m[ci][0];
-          var cx     = root[0]+(ci   +1)*psh.colshift;
-          var cy     = root[1]-(level+1)*psh.levelshift;
+          var cx     = rootx_pixel     +(ci   +1)*psh.colshift;
+          var cy     = rooty_pixel[mmi]-(level+1)*psh.levelshift;
           
           // stroke circle
           ctx.strokeStyle="rgba(0,0,0,0.3)";
@@ -122,12 +186,12 @@ var drawTree=function(mm){
           // stroke branch
           var branchr = psh.levelshift-psh.radius; // radius of branch
           var pi      = findParent(m,ci);     // parent
-          var px     = root[0]+(pi      +1)*psh.colshift;
+          var px     = rootx_pixel+(pi      +1)*psh.colshift;
           var py;
           if(pi>=0){
-              py     = root[1]-(m[pi][0]+1)*psh.levelshift;
+              py     = rooty_pixel[mmi]-(m[pi][0]+1)*psh.levelshift;
           }else{//branch for root
-              py     = root[1];
+              py     = rooty_pixel[mmi];
           }
           ctx.strokeStyle='black';
           ctx.lineWidth  = 1;
@@ -143,14 +207,11 @@ var drawTree=function(mm){
           
           // stroke text
           text   = String(m[ci][1]);
-          ctx.strokeStyle='black';
+          ctx.fillStyle='black';
           ctx.font = String(psh.fontsize)+'px Segoe UI';
           textwidth  = ctx.measureText(text).width;
           textheight = psh.fontsize;
-          ctx.fillText(text,cx-textwidth/2,cy+textheight/2*0.7);
-          
-          debug.value=debug.value+"ci="+String(ci)+" cx="+String(cx)+" cy="+String(cy);
-          debug.value=debug.value+"\n";
+          ctx.fillText(text,cx-textwidth/2,cy+textheight/2*0.7);          
         }//for ci
       }//for mmi
     break;// pair sequence hydra
